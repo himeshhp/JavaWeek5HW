e053a154867bb5259ec48ac7add35e692da0d6c5
vaibhdoshi <vaibhdoshi@gmail.com>
2023-01-26 22:21:04 +0000

    This is my second commit

--- src/javaweek5hw/GITPractice.java
+++ src/javaweek5hw/GITPractice.java
@@ -3,11 +3,11 @@ package javaweek5hw;
 public class GITPractice {
     public static void main(String[] args) {
         System.out.println("Himesh");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
         System.out.println("Vaibhavi");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
         System.out.println("");
     }
 }
